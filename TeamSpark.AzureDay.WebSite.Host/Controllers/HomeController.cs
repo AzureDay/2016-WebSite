995f39486d14503c1155dcee6e30857f5281416a
Boyko Anton <boyko.ant@live.com>
2015-10-11 22:52:13 +0300

    initialize solution

---
+++ TeamSpark.AzureDay.WebSite.Host/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TeamSpark.AzureDay.WebSite.Host.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
