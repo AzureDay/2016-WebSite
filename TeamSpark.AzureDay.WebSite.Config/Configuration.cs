5f8a4214dcfcb78a7f5ec11076ffaf7b7cdcc1a7
Boyko Anton <boyko.ant@live.com>
2016-05-10 12:49:00 +0300

    add some configs

---
+++ TeamSpark.AzureDay.WebSite.Config/Configuration.cs
@@ -0,0 +1,28 @@
+﻿namespace TeamSpark.AzureDay.WebSite.Config
+{
+	public static class Configuration
+	{
+		#region general
+
+		public static string Year
+		{
+			get { return "2016"; }
+		}
+
+		#endregion
+
+		#region storage
+
+		public static string AccountName
+		{
+			get { return ""; }
+		}
+
+		public static string AccountKey
+		{
+			get { return ""; }
+		}
+
+		#endregion
+	}
+}
