259de16e8d8f92a63fa7747ed658e82c7046830d
Boyko Anton <boyko.ant@live.com>
2016-05-13 14:12:36 +0300

    introduce speakers in cli admin

--- TeamSpark.AzureDay.WebSite.CLI/Program.cs
+++ TeamSpark.AzureDay.WebSite.CLI/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace TeamSpark.AzureDay.WebSite.CLI
+﻿using System;
+using TeamSpark.AzureDay.WebSite.CLI.Data;
+using TeamSpark.AzureDay.WebSite.Data;
+
+namespace TeamSpark.AzureDay.WebSite.CLI
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			//DataFactory.InitializeAsync().Wait();
+
+			Speaker.Add();
+
+			Console.WriteLine("Press 'enter' to close.");
+			Console.ReadLine();
 		}
 	}
 }
