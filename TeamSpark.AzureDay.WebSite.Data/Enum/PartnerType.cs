5d16dce2a939c18c633e76de81ad30963fedfd41
Boyko Anton <boyko.ant@live.com>
2016-05-17 20:16:05 +0300

    introduce partners types on UI

--- TeamSpark.AzureDay.WebSite.Data/Enum/PartnerType.cs
+++ TeamSpark.AzureDay.WebSite.Data/Enum/PartnerType.cs
@@ -9,4 +9,28 @@
 		Info = 501,
 		Speaker = 601
 	}
+
+	public static class PartnerTypeExtension
+	{
+		public static string ToDisplayString(this PartnerType type)
+		{
+			switch (type)
+			{
+				case PartnerType.VIP:
+					return "Генеральный партнер";
+				case PartnerType.Gold:
+					return "Золотые партнеры";
+				case PartnerType.Silver:
+					return "Серебрянные партнеры";
+				case PartnerType.Raffle:
+					return "Партнеры по призам";
+				case PartnerType.Info:
+					return "Информационные партнеры";
+				case PartnerType.Speaker:
+					return "Партнеры по докладчикам";
+				default:
+					return string.Empty;
+			}
+		}
+	}
 }
