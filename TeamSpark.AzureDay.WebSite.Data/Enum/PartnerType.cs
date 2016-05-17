4c48c0b0a69a3a8151d6202d2d87e8d92763b5ff
Boyko Anton <boyko.ant@live.com>
2016-05-17 20:01:54 +0300

    introduce partner entity in data and app layers

---
+++ TeamSpark.AzureDay.WebSite.Data/Enum/PartnerType.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamSpark.AzureDay.WebSite.Data.Enum
+{
+	public enum PartnerType
+	{
+		VIP = 101,
+		Gold = 201,
+		Silver = 301,
+		Raffle = 401,
+		Info = 501,
+		Speaker = 601
+	}
+}
