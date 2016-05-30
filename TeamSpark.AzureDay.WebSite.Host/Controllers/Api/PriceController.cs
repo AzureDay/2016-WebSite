4baa15cefbd8d62dc7261bc1c9fd1eb1925a3806
Boyko Anton <boyko.ant@live.com>
2016-05-31 00:07:18 +0300

    introduced payment system integration

--- TeamSpark.AzureDay.WebSite.Host/Controllers/Api/PriceController.cs
+++ TeamSpark.AzureDay.WebSite.Host/Controllers/Api/PriceController.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Kaznachey.KaznacheyPayment;
+using Microsoft.ApplicationInsights;
 using TeamSpark.AzureDay.WebSite.App;
 using TeamSpark.AzureDay.WebSite.Data.Enum;
+using TeamSpark.AzureDay.WebSite.Notification;
+using TeamSpark.AzureDay.WebSite.Notification.Email.Model;
 
 namespace TeamSpark.AzureDay.WebSite.Host.Controllers.Api
 {
@@ -24,5 +29,79 @@ namespace TeamSpark.AzureDay.WebSite.Host.Controllers.Api
 
 			return ticketPrice;
 		}
-    }
+
+		[HttpPost]
+		[Route("api/tickets/paymentconfirm")]
+		public async Task<string> PaymentConfirm([FromBody]PaymentResponse response)
+		{
+			var email = response.MerchantInternalUserId;
+			var ticket = await AppFactory.TicketService.Value.GetTicketByEmailAsync(email);
+
+			if (ticket != null)
+			{
+				if (response.ErrorCode != 0)
+				{
+					var ai = new TelemetryClient();
+
+					var props = new Dictionary<string, string>();
+					props.Add("MerchantInternalPaymentId", response.MerchantInternalPaymentId);
+					props.Add("MerchantInternalUserId", response.MerchantInternalUserId);
+					props.Add("OrderId", response.OrderId.ToString());
+					props.Add("ErrorCode", response.ErrorCode.ToString());
+					props.Add("DebugMessage", response.DebugMessage);
+					props.Add("Sum", response.Sum.ToString("F"));
+					props.Add("OrderSum", response.OrderSum.ToString("F"));
+
+					ai.TrackEvent("Payment error", props);
+
+					var attendee = await AppFactory.AttendeeService.Value.GetAttendeeByEmailAsync(email);
+
+					var message = new ErrorPaymentModel
+					{
+						Email = attendee.EMail,
+						FullName = attendee.FullName
+					};
+
+					await Task.WhenAll(
+						AppFactory.TicketService.Value.DeleteTicketAsync(email),
+						NotificationFactory.AttendeeNotificationService.Value.SendPaymentErrorEmailAsync(message)
+					);
+				}
+				else
+				{
+					ticket.IsPayed = true;
+
+					var attendee = await AppFactory.AttendeeService.Value.GetAttendeeByEmailAsync(email);
+
+					var message = new ConfirmPaymentModel
+					{
+						Email = attendee.EMail,
+						FullName = attendee.FullName
+					};
+
+					await Task.WhenAll(
+						AppFactory.TicketService.Value.SetTicketPayedAsync(email),
+						NotificationFactory.AttendeeNotificationService.Value.SendPaymentConfirmationEmailAsync(message)
+					);
+				}
+			}
+			else
+			{
+				var ai = new TelemetryClient();
+
+				var props = new Dictionary<string, string>();
+				props.Add("MerchantInternalPaymentId", response.MerchantInternalPaymentId);
+				props.Add("MerchantInternalUserId", response.MerchantInternalUserId);
+				props.Add("OrderId", response.OrderId.ToString());
+				props.Add("ErrorCode", response.ErrorCode.ToString());
+				props.Add("DebugMessage", response.DebugMessage);
+				props.Add("Sum", response.Sum.ToString("F"));
+				props.Add("OrderSum", response.OrderSum.ToString("F"));
+
+				ai.TrackEvent("Unable to find ticket", props);
+			}
+
+			return string.Empty;
+		}
+	}
 }
