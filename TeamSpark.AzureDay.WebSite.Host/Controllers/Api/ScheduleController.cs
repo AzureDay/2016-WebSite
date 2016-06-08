5eec5579ef829b25f85550193442bbf8f0af90e2
Boyko Anton <boyko.ant@live.com>
2016-06-08 11:40:56 +0300

    schedule builder api controller

---
+++ TeamSpark.AzureDay.WebSite.Host/Controllers/Api/ScheduleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TeamSpark.AzureDay.WebSite.Host.Controllers.Api
+{
+    public class ScheduleController : ApiController
+    {
+		[HttpGet]
+		[Route("api/schedule/selected")]
+		//[Authorize]
+	    public async Task<List<string>> GetSelectedTopics()
+		{
+			return new List<string>();
+		}
+
+		[HttpPost]
+		[Route("api/schedule/{topicId}")]
+		//[Authorize]
+		public async Task SelectTopic(string topicId)
+		{
+			
+		}
+
+		[HttpDelete]
+		[Route("api/schedule/{topicId}")]
+		//[Authorize]
+		public async Task UnselectTopic(string topicId)
+		{
+
+		}
+	}
+}
