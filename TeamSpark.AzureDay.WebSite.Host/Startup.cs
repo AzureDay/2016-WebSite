6295f043130f4c11b87a10813123a75f1e131751
Ihor Leontiev <leontuev@rambler.ru>
2016-06-15 22:50:44 +0000

    Add Web Api Token-based Autentification

---
+++ TeamSpark.AzureDay.WebSite.Host/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(TeamSpark.AzureDay.WebSite.Host.Startup))]
+
+namespace TeamSpark.AzureDay.WebSite.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            OAuthAuthorizationServerOptions OAuthOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/api/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(2),
+                Provider = new AuthorizationServerProvider()
+            };
+
+            // To Generate token
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
