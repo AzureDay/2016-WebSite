065d299b697ad778b25cef70ce193a5fa20b382f
gleamstar <gleamstar@outlook.com>
2016-06-16 23:29:09 +0300

    Added Attendee controller

---
+++ TeamSpark.AzureDay.WebSite.Host/Controllers/Api/AttendeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TeamSpark.AzureDay.WebSite.App;
+
+namespace TeamSpark.AzureDay.WebSite.Host.Controllers.Api
+{
+    public class AttendeeController : ApiController
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("api/attendee/info")]
+        public async Task<IHttpActionResult> GetAttendeeByEmail()
+        {
+            var score = await AppFactory.AttendeeService.Value.GetAttendeeByEmailAsync(RequestContext.Principal.Identity.Name);
+
+            return Ok(score);
+        }
+    }
+}
